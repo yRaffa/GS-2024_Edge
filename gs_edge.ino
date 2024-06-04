7001ccc2f37eb121b9ed1788905433b69d26843f
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-04 12:37:38 -0300

    Update GS

--- gs_edge.ino
+++ gs_edge.ino
@@ -1,9 +1,14 @@
-// Incluindo de bibliotecas
-#include <EEPROM.h>
-#include <LiquidCrystal.h>
+// Incluindo bibliotecas
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // Declarando variaves das portas de cada componente
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+const float BETA = 3950;
+int temperatura = A2;
+
+int potenciometro = A3;
 
 int ledRed = 8;
 int ledYellow = 9;
@@ -15,24 +20,31 @@ int echo = 12;
 int trigger = 13;
 
 // Declarando variaveis
+int valor = 0;
 int porcentagem = 0;
 float cm = 0;
+float ph = 0;
+float celsius = 0;
 
 // Função para apagar linha do lcd
-void apagarLinha(int linha) {
-  lcd.setCursor(0, linha);
-  for(int i = 0; i < 16; i++)
+void apagarLinha(int coluna, int linha) {
+  lcd.setCursor(coluna, linha);
+  for(int i = coluna; i < (20 - coluna); i++)
     lcd.print(" ");
-  lcd.setCursor(0, linha);
+  lcd.setCursor(coluna, linha);
 }
 
-// Iniciando do arduido
+// Iniciando o arduido
 void setup() {
 
   // Iniciando o serial
   Serial.begin(9600);
 
   // Declarando pinModes de cada componente
+  pinMode(temperatura, INPUT);
+
+  pinMode(potenciometro, INPUT);
+
   pinMode(ledRed, OUTPUT);
   pinMode(ledYellow, OUTPUT);
   pinMode(ledGreen, OUTPUT);
@@ -43,11 +55,8 @@ void setup() {
   pinMode(trigger, OUTPUT);
 
   // Iniciando lcd
-  lcd.begin(16, 2);
-
-  // Printando informacao no lcd
-  lcd.setCursor(0, 0);
-  lcd.print("Tanque:");
+  lcd.init();
+  lcd.backlight();
 }
 
 // Loop do arduino
@@ -69,41 +78,30 @@ void loop() {
   porcentagem = (porcentagem - 100);
   porcentagem = (porcentagem * -1);
 
-  // Printando a porcentagem no lcd 
+  // Simulando ph no potenciometro
+  ph = map(analogRead(potenciometro), 0, 1023, 0.0, 14.0);
+
+  // Captando dados do sensor de temperatura
+  valor = analogRead(temperatura);
+
+  // Transformando valor para °C
+  celsius = 1 / (log(1 / (1023. / valor - 1)) / BETA + 1.0 / 298.15) - 273.15;  
+
+// --- LCD --- //
+
+  // Printando a porcentagem no lcd
+  lcd.setCursor(0, 0);
+  lcd.print("Tanque:");
+  apagarLinha(7, 0);
   lcd.setCursor(7, 0);
   lcd.print(porcentagem);
   lcd.print("%");
-
-  String str_porcentagem = String(porcentagem);
-
-  if(str_porcentagem.length() < 3) {
-    lcd.setCursor(10, 0);
-    lcd.print(' ');
-  } 
-
-  if(str_porcentagem.length() < 2) {
-    lcd.setCursor(9, 0);
-    lcd.print(' ');
-    lcd.setCursor(10, 0);
-    lcd.print(' ');
-  }
-
-  // Printando distancia e porcentagem no serial
-  Serial.print("Distancia: ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.print(" || ");
-  Serial.print("Porcentagem: ");
-  Serial.print(porcentagem);
-  Serial.print("%");
-  Serial.print("\n");
-  delay(1000);
-
+    
   // Bloco de comando para estado 'Tanque Cheio'
   if(porcentagem > 99) {
 
     // Apagando a linha e printando 'Tanque Cheio'
-    apagarLinha(1);
+    apagarLinha(0, 1);
     lcd.setCursor(0, 1);
     lcd.print("Tanque Cheio");
 
@@ -112,12 +110,12 @@ void loop() {
     digitalWrite(ledYellow, LOW);
     digitalWrite(ledGreen, HIGH);
   }
-  
+    
   // Bloco de comando para estado 'Quase Cheio'
   else if(porcentagem > 75) {
 
     // Apagando a linha e printando 'Quase Cheio'
-    apagarLinha(1);
+    apagarLinha(0, 1);
     lcd.setCursor(0, 1);
     lcd.print("Quase Cheio");
 
@@ -133,9 +131,9 @@ void loop() {
   else if(porcentagem > 25) {
 
     // Apagando a linha e printando 'Combustivel OK'
-    apagarLinha(1);
+    apagarLinha(0, 1);
     lcd.setCursor(0, 1);
-    lcd.print("Combustivel OK");
+    lcd.print("Combustivel Ok");
 
     // Gerenciando leds
     digitalWrite(ledRed, LOW);
@@ -147,7 +145,7 @@ void loop() {
   else if(porcentagem > 1) {
 
     // Apagando a linha e printando 'Quase Vazio'
-    apagarLinha(1);
+    apagarLinha(0, 1);
     lcd.setCursor(0, 1);
     lcd.print("Quase Vazio");
 
@@ -166,7 +164,7 @@ void loop() {
   else {
 
     // Apagando a linha e printando 'Tanque Vazio'
-    apagarLinha(1);
+    apagarLinha(0, 1);
     lcd.setCursor(0, 1);
     lcd.print("Tanque Vazio");
 
@@ -175,4 +173,95 @@ void loop() {
     digitalWrite(ledYellow, LOW);
     digitalWrite(ledGreen, LOW);
   }
+
+
+  // Printando Ph no lcd
+  lcd.setCursor(0, 2);
+  lcd.print("Ph:");
+  apagarLinha(3, 2);
+  lcd.setCursor(3, 2);
+  lcd.print(ph, 0);
+
+  // Bloco de comando para estado 'Ph Ok'
+  if(ph >= 6 && ph <= 8) {
+
+    // Apagando a linha e printando estado ph 'PH OK'
+    apagarLinha(0, 3);
+    lcd.setCursor(0, 3);
+    lcd.print("Ph Ok");
+  } 
+  
+  // Bloco de comando para estado 'Ph Baixo'
+  else if(ph < 6) {
+
+    // Apagando a linha e printando estado ph 'PH BAIXO'
+    apagarLinha(0, 3);
+    lcd.setCursor(0, 3);
+    lcd.print("Ph Baixo");
+  }
+
+  // Bloco de comando para estado 'Ph Alto'
+  else if(ph > 8) {
+
+    // Apagando a linha e printando estado ph 'PH ALTO'
+    apagarLinha(0, 3);
+    lcd.setCursor(0, 3);
+    lcd.print("Ph Alto");
+  }
+
+
+  // Printando temperatura no lcd
+  lcd.setCursor(10, 2);
+  lcd.print("Temp:");
+  apagarLinha(15, 2);
+  lcd.setCursor(15, 2);
+  lcd.print(celsius, 0);
+  lcd.print("C");
+  
+  // VALORES FICTICIOS DE TEMPERATURA PARA TESTES COM SENSOR (SÓ VAI ATÉ 80°C)
+
+  // Bloco de comando para estado 'Temp Ok'
+  if(celsius >= 20 && celsius <= 60) {
+
+    // Apagando a linha e printando estado de temperatura 'Temp Ok'
+    apagarLinha(10, 3);
+    lcd.setCursor(10, 3);
+    lcd.print("Temp Ok");
+  }
+
+  // Bloco de comando para estado 'Temp Baixa'
+  else if(celsius < 20) {
+
+    // Apagando a linha e printando estado de temperatura 'Temp Baixa'
+    apagarLinha(10, 3);
+    lcd.setCursor(10, 3);
+    lcd.print("Temp Baixa");
+  }
+
+  // Bloco de comando para estado 'Temp Alta'
+  else if(celsius > 60) {
+
+    // Apagando a linha e printando estado de temperatura 'Temp Alta'
+    apagarLinha(10, 3);
+    lcd.setCursor(10, 3);
+    lcd.print("Temp Alta");
+  }
+
+  // Printando dados no serial
+  Serial.print("Distancia: ");
+  Serial.print(cm, 0);
+  Serial.print("cm");
+  Serial.print(" || ");
+  Serial.print("Tanque: ");
+  Serial.print(porcentagem);
+  Serial.print("%");
+  Serial.print(" || ");
+  Serial.print("Ph: ");
+  Serial.print(ph, 0);
+  Serial.print(" || ");
+  Serial.print("Temperatura: ");
+  Serial.print(celsius, 0);
+  Serial.print("°C");
+  Serial.print("\n");
+  delay(1000);
 }
